9eb92ec9de0c8364dface2dbdc78ba2398fadbac
Ivazbit <99424267+Ivazbit@users.noreply.github.com>
2022-05-20 11:28:10 +0300

    Delete vu_lab4 directory

--- vu_lab4/app/src/main/java/com/example/todoapp/ui/theme/HomeViewState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.todoapp.ui.theme
-
-import com.example.todoapp.data.Todo
-
-data class HomeViewState (
-    val todoList: List<Todo> = emptyList(),
-    val selected: Boolean = false
-)
